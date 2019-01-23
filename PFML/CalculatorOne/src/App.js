524bf0c353c6859153cf8d20a02df92bc9b2381a
Minghua Sun <clairesunstudio@gmail.com>
2019-01-23 18:10:12 -0500

    initial commit for calculator one

---
+++ PFML/CalculatorOne/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Header, Footer } from '@massds/mayflower-react';
+import UtilityNavData from './data/UtilityNav.data';
+import MainNavData from './data/MainNav.data';
+import HeaderSearchData from './data/HeaderSearch.data';
+import FooterLinksLiveData from './data/FooterLinksLive.json';
+import SocialLinksLiveData from './data/SocialLinksLive.json';
+
+import './index.css';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.footerProps = {
+      footerLinks: FooterLinksLiveData.footerLinks,
+      socialLinks: SocialLinksLiveData.socialLinks
+    };
+    this.headerProps = {
+      utilityNav: UtilityNavData,
+      headerSearch: HeaderSearchData,
+      mainNav: MainNavData,
+      hideHeaderSearch: true,
+      hideBackTo: true,
+      siteLogoDomain: { url: { domain: 'https://www.mass.gov/' } }
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header {...this.headerProps} />
+          <main className="main-content">
+            <section className="main-content--two">
+              <div className="ma__page-header__content">
+                <h1 className="ma__page-header__title">Mayflower Starter Template</h1>
+              </div>
+            </section>
+          </main>
+        <Footer {...this.footerProps} />
+      </div>
+    );
+  }
+}
+
+export default App;
