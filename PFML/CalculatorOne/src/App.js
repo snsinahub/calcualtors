7951ca7facdcd3f3d6a60eb8c8dece7fce0147ed
Minghua Sun <clairesunstudio@gmail.com>
2019-01-24 09:58:54 -0500

    add first fieldset

--- PFML/CalculatorOne/src/App.js
+++ PFML/CalculatorOne/src/App.js
@@ -33,25 +33,64 @@ class App extends Component {
             <section className="main-content--two">
               <div className="ma__page-header__content">
                 <h1 className="ma__page-header__title">Paid Family Medical Leave Contribution Caculator</h1>
-                <form action="/action_page.php">
-                  <div class="ma__input-group ">
-                    <div class="ma__input-group__title">
-                      Do you have any employees in Massachusetts?
-                      <div class="ma__input-group__items ma__input-group__items--inline">
-                        <div class="ma__input-group__item">
-                          <span class="ma__input-radio">
-                            <input name="ma-employee" type="radio" value="yes" id="ma-employee-yes" />
-                            <label for="ma-employee-yes"><span>Yes</span></label>
-                          </span>
-                          <span class="ma__input-radio">
-                            <input name="ma-employee" type="radio" value="no" id="ma-employee-no" />
-                            <label for="ma-employee-no"><span>No</span></label>
-                          </span>
+                <div className="page-content">
+                  <form class="ma__form-page" action="#">
+                    <fieldset>
+                      <div class="ma__input-group">
+                        <div class="ma__input-group__title">
+                          Do you have any employees in Massachusetts?
+                          <div class="ma__input-group__items ma__input-group__items--inline">
+                            <div class="ma__input-group__item">
+                              <span class="ma__input-radio">
+                                <input name="ma-employee" type="radio" value="yes" id="ma-employee-yes" />
+                                <label for="ma-employee-yes"><span>Yes</span></label>
+                              </span>
+                            </div>
+                            <div class="ma__input-group__item">
+                              <span class="ma__input-radio">
+                                <input name="ma-employee" type="radio" value="no" id="ma-employee-no" />
+                                <label for="ma-employee-no"><span>No</span></label>
+                              </span>
+                            </div>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </div>
-                </form>
+                      <div class="ma__input-group--inline">
+                        <label
+                          for="number-input"
+                          class="ma__label ma__label--inline ma__label--required ">How many of your MA employees receive W2s?
+                        </label>
+                        <input
+                          class="ma__input js-is-required"
+                          name="number-input"
+                          id="number-input"
+                          type="number"
+                          placeholder="type something"
+                          data-type="number"
+                          maxlength="16"
+                            pattern="[0-9]*"
+                          required />
+                      </div>
+                      <div class="ma__input-group--inline">
+                        <label
+                          for="number-input"
+                          class="ma__label ma__label--inline ma__label--required ">How many of your MA employees receive W2s?
+                        </label>
+                        <input
+                          class="ma__input js-is-required"
+                          name="number-input"
+                          id="number-input"
+                          type="number"
+                          placeholder="type something"
+                          data-type="number"
+                          maxlength="16"
+                            pattern="[0-9]*"
+                          required />
+                      </div>
+                    </fieldset>
+                    <hr />
+                  </form>
+                </div>
               </div>
             </section>
           </main>
