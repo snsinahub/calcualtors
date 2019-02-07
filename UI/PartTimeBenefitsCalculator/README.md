e3d8c8bee20d0c34b85030175e5ccbd72e95e152
Minghua Sun <clairesunstudio@gmail.com>
2019-02-07 11:01:55 -0500

    initial commit for UI repo

---
+++ UI/PartTimeBenefitsCalculator/README.md
@@ -0,0 +1,27 @@
+# UI: Unemployment Insurance Benefits Calculator for Part-time Workers
+
+Welcome to the Commonwealth of Massachusetts' Single Page Application (SPA), powering the "Unemployment Insurance Benefits Calculator for Part-time Workers" . This is a static website built with React, [Mayflower React](https://github.com/massgov/mayflower/) (Commonwealth's Design System), and our [Mayflower React Starter](https://github.com/massgov/mayflower-react-starter/). The [Mayflower React Starter](https://github.com/massgov/mayflower-react-starter/) is a customized instance of [Facebook's Create React App](https://github.com/facebook/create-react-app).
+
+## Quick start
+
+- $ ``git clone https://github.com/massgov/Mayflower-React-Starter.git``
+- $ ``cd Mayflower-React-Starter``
+- $ ``npm i``
+- $ ``npm start``
+- Browse to http://localhost:3000, or the custom port shown in the output of npm start.
+
+## System Requirements
+
+- node.js, currently standardized on version 8.9.4
+- npm, currently standardized on version 10.10.0
+- To run tests locally, you will need to install [Watchman](https://facebook.github.io/watchman/docs/install.html).
+- That's it! All other dependencies should be included when you run ``npm i``.
+
+## Production build
+- $ ``npm run build`` (Builds production app)
+
+## Other Handy Commands
+- $ ``npm run lint`` (Runs linter)
+- $ ``npm run lint-fix`` (Runs linter and fixes what it can automagically)
+- $ ``npm run build && serve`` (Serves production app after built)
+- $ ``npm run build-css`` (Compiles css files from scss)
---
+++ UI/PartTimeBenefitsCalculator/src/data/MainNav.data.js
@@ -0,0 +1,161 @@
+const mainNav = [
+  {
+    href: 'https://www.mass.gov/topics/living',
+    text: 'Living',
+    active: false,
+    subNav: [
+      {
+        href: 'https://www.mass.gov/topics/health-social-services',
+        text: 'Health & Social Services'
+      },
+      {
+        href: 'https://www.mass.gov/topics/families-children',
+        text: 'Family & Children'
+      },
+      {
+        href: 'https://www.mass.gov/topics/housing-property',
+        text: 'Housing & Property'
+      },
+      {
+        href: 'https://www.mass.gov/topics/transportation',
+        text: 'Transportation'
+      },
+      {
+        href: 'https://www.mass.gov/topics/legal-justice',
+        text: 'Legal & Justice'
+      },
+      {
+        href: 'https://www.mass.gov/topics/energy',
+        text: 'Energy'
+      },
+      {
+        href: 'https://www.mass.gov/topics/environment',
+        text: 'Environment'
+      },
+      {
+        href: 'https://www.mass.gov/topics/taxes',
+        text: 'Taxes'
+      },
+      {
+        href: 'https://www.mass.gov/topics/unclaimed-property',
+        text: 'Unclaimed Property'
+      }
+    ]
+  },
+  {
+    href: 'https://www.mass.gov/topics/working',
+    text: 'Working',
+    active: false,
+    subNav: [
+      {
+        href: 'https://www.mass.gov/topics/business-resources',
+        text: 'Business Resources'
+      },
+      {
+        href: 'https://www.mass.gov/topics/professional-licenses-permits',
+        text: 'Professional Licenses & Permits'
+      },
+      {
+        href: 'https://www.mass.gov/topics/unemployment',
+        text: 'Unemployment'
+      },
+      {
+        href: 'https://www.mass.gov/topics/finding-a-job',
+        text: 'Finding a Job'
+      },
+      {
+        href: 'https://www.mass.gov/topics/taxes',
+        text: 'Taxes'
+      },
+      {
+        href: 'https://www.mass.gov/topics/workers-rights-safety',
+        text: 'Workers\' Rights & Safety'
+      },
+      {
+        href: 'https://www.mass.gov/topics/industry-regulations',
+        text: 'Industry Regulations'
+      },
+      {
+        href: 'https://www.mass.gov/topics/for-state-employees',
+        text: 'For State Employees'
+      }
+    ]
+  },
+  {
+    href: 'https://www.mass.gov/topics/learning',
+    text: 'Learning',
+    active: false,
+    subNav: [
+      {
+        href: 'https://www.mass.gov/topics/early-childhood-education-care',
+        text: 'Early Childhood Education & Care'
+      },
+      {
+        href: 'https://www.mass.gov/topics/elementary-secondary-schools',
+        text: 'Elementary & Secondary Schools'
+      },
+      {
+        href: 'https://www.mass.gov/topics/higher-education',
+        text: 'Higher Education'
+      },
+      {
+        href: 'https://www.mass.gov/topics/continuing-education',
+        text: 'Continuing Education'
+      },
+      {
+        href: 'https://www.mass.gov/topics/for-educators-administrators',
+        text: 'For Educators & Administrators'
+      }
+    ]
+  },
+  {
+    href: 'https://www.mass.gov/topics/visiting-exploring',
+    text: 'Visiting & Exploring',
+    active: false,
+    subNav: [
+      {
+        href: 'https://www.mass.gov/topics/parks-recreation',
+        text: 'Parks & Recreation'
+      },
+      {
+        href: 'https://www.mass.gov/topics/visiting-massachusetts',
+        text: 'Visiting Massachusetts'
+      },
+      {
+        href: 'https://www.mass.gov/topics/hunting-fishing',
+        text: 'Hunting & Fishing'
+      }
+    ]
+  },
+  {
+    href: 'https://www.mass.gov/topics/your-government',
+    text: 'Your Government',
+    active: false,
+    subNav: [
+      {
+        href: 'https://www.mass.gov/topics/executive-branch',
+        text: 'Executive'
+      },
+      {
+        href: 'https://www.mass.gov/topics/constitutionals-independents',
+        text: 'Constitutionals & Independents'
+      },
+      {
+        href: 'https://www.mass.gov/topics/legislative-branch',
+        text: 'Legislative'
+      },
+      {
+        href: 'https://www.mass.gov/topics/judicial-branch',
+        text: 'Judicial'
+      },
+      {
+        href: 'https://www.mass.gov/state-a-to-z',
+        text: 'State Offices & Courts A-Z'
+      }
+    ]
+  }
+];
+
+export default {
+  mainNav
+};
